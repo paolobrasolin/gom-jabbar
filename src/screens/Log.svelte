dd8abf67690f7ac3e791a10ee1699e6272758589
Paolo Brasolin <paolo.brasolin@gmail.com>
2026-09-14 01:08:13 +0200

    Use scrollLeft when resetting chip rows (jsdom has no scrollTo)

--- src/screens/Log.svelte
+++ src/screens/Log.svelte
@@ -66,7 +66,7 @@
   function reset() {
     draft = emptyDraft({ ongoing: draft.ongoing, pain: draft.readings[PAIN] ?? 5 })
     detailsOpen = false
-    document.querySelectorAll<HTMLElement>('.form .chips').forEach((el) => el.scrollTo({ left: 0 }))
+    document.querySelectorAll<HTMLElement>('.form .chips').forEach((el) => (el.scrollLeft = 0))
   }
 
   async function save() {
